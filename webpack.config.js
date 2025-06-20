d9c9693d7b39c99866d3b58191caf002a1c8c74f
ouvax <axel.ouvrard12@gmail.com>
2025-06-21 00:01:15 +0200

    Mise à jour : corrections et build webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.resolve.fallback = {
+    ...(config.resolve.fallback || {}),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    vm: false, // ← ici on désactive 'vm' pour éviter l'erreur
+  };
+
+  return config;
+};
