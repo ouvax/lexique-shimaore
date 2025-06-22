18332b43f4d16de7d34a06cf7ed94d76c441ba23
ouvax <axel.ouvrard12@gmail.com>
2025-06-22 23:39:47 +0200

    ✅ Mise à jour Firebase config et LoginScreen

--- firebase.ts
+++ firebase.ts
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: '...',
-  authDomain: '...',
-  projectId: '...',
-  storageBucket: '...',
-  messagingSenderId: '...',
-  appId: '...',
+  apiKey: 'AIzaSyDgRi7et35moEGfs1OPmPVeBOu_jTOCklU',
+  authDomain: 'lexique-shimaore.firebaseapp.com',
+  projectId: 'lexique-shimaore',
+  storageBucket: 'lexique-shimaore.appspot.com',
+  messagingSenderId: '842167679041',
+  appId: '1:842167679041:web:a73c12a66e02a590c9eec0',
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
